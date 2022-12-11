18bd535c1ebe9c67aed9e5395b061eec3f16c103
XxANTONIO73xX <60475171+XxANTONIO73xX@users.noreply.github.com>
2022-12-11 13:10:11 -0700

    up

--- grafos/ejercicio3.py
+++ grafos/ejercicio3.py
@@ -6,8 +6,6 @@ En esta clase crean un gráfico con los nodos y bordes dados, y luego llama a la
 
 def ejercicio3():
     
-    print("------------------------- ejecicio numero 3 -------------------------")
-    
     grafo3 = Grafo()
     grafo3.agregarVector('A', 0)  
     grafo3.agregarVector('B', 4)
@@ -37,26 +35,20 @@ def ejercicio3():
     
     #C
     grafo3.relacionar('C', 'F', 3)
-    grafo3.relacionar('C', 'G', 8)
-    
+    grafo3.relacionar('C', 'G', 8)    
     #D
     grafo3.relacionar('D', 'H', 5)
-    grafo3.relacionar('D', 'I1', 4)
-    
+    grafo3.relacionar('D', 'I1', 4)   
     #E
-    grafo3.relacionar('E', 'I2', 1)
-    
+    grafo3.relacionar('E', 'I2', 1)    
     #F
     grafo3.relacionar('F', 'I3', 4)
-    grafo3.relacionar('F', 'J1', 4)
-    
+    grafo3.relacionar('F', 'J1', 4)    
     #G
     grafo3.relacionar('G', 'J2', 2)
-    grafo3.relacionar('G', 'K', 2)
-    
+    grafo3.relacionar('G', 'K', 2)  
     #H
-    grafo3.relacionar('H', 'L', 5)
-    
+    grafo3.relacionar('H', 'L', 5)    
     #L
     grafo3.relacionar('L', 'M', 7)
 
--- grafos/prioridad.py
+++ grafos/prioridad.py
@@ -1,16 +1,28 @@
 from Lista import *
-
+"""
+Está clase hereda de la clase lista.
+Contiene las funciones de insertar, eliminar y ordendar
+"""
 class Prioridad(Lista):
     def __init__(self, dato, costo):
         Lista.__init__(self, dato, costo)
         self.tamanio = 1
 
     def insertar(self, dato, costo):
+        """
+        La función insertar() toma dos argumentos, dato y costo, 
+        y devuelve el valor de la función insertarNodo() si no es False
+        
+        """
         respuesta = self.insertarNodo(dato, costo)
         if respuesta != False:
             self.tamanio += 1
     
     def eliminar(self):
+        """
+        Elimina el primer nodo de la lista y devuelve los datos de ese nodo
+        :return: Los datos del nodo que se eliminó.
+        """
         dato = self.eliminarNodoCabeza()
         if dato:
             self.tamanio -= 1
@@ -19,6 +31,10 @@ class Prioridad(Lista):
             return False
 
     def ordenar(self):
+        """
+        Ordena una lista enlazada intercambiando los nodos de posicion
+        :return: La lista ordenada.
+        """
         #print("----sin ordenar----")
         #self.recorrerLista()
         fin = None
@@ -47,12 +63,23 @@ class Prioridad(Lista):
     
 
 def buscarLista(arreglo, nodoPadre):
+    """
+    Devuelve la lista del array que tiene el mismo nodo principal que el nodo pasado como parámetro.
+    
+    :return: La lista que contiene el nodo que es el padre del nodo que se busca.
+    """
     for lista in arreglo:
         if lista.NodoCabeza.getDato() == nodoPadre.getDato():
             return lista
 
 
 def costoUniforme(arreglo, meta):
+    """
+    Esta función realiza la búsqueda de costo uniforme.
+    
+    :param arreglo: es un array de listas
+    :param meta: el nodo meta
+    """
     #empieza por el nodo raiz
     nodoAux = arreglo[0].NodoCabeza
     #se agrega a la lista con caracteristica de prioridad
